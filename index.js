37a45341152f769ae9740932066f52fd31ff5597
yunisuthn <yunis.thn@gmail.com>
2021-05-27 14:06:23 +0300

    test5

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require("path");
 const fs = require('fs');
 
-if(process.platform == "win32") { dir_home = "USERPROFILE"}
-if(process.platform  != "w32") {dir_home= "HOME"}
+if(process.platform == "win32") { dir_home = process.env["USERPROFILE"]}
+if(process.platform  != "win32") {dir_home= process.env["HOMME"]}
 // var dir_home = process.env[process.platform !="win32"?"HOME":"USERPROFILE"];
 
 console.log("process.platform == ", dir_home);
 
-var rep = path.join(dir_home,"Desktop", "Geeks")
+var rep = path.resolve(dir_home,"Desktop", "Geeks")
 if (!fs.existsSync(rep)){
   fs.mkdirSync(rep);
   //fs.mkdirSync(dir);
