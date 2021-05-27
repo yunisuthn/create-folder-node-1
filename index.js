56e23eecf218053e79b2509c13f8a6c4a6bb1993
yunisuthn <47241138+yunisuthn@users.noreply.github.com>
2021-05-27 11:47:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const path = require("path");
 const fs = require('fs');
 var dir_home = process.env[process.platform =="win32"?"USERPROFILE":"HOME"];
 
-console.log("process.platform ");
-console.log("process.platform == ", process.platform);
-
-
 //fs.mkdirSync(path.join(dir_home,"Desktop", "Geeks"));
 app = express()
 
 
 
 app.use(function (req, res) {
+  console.log("process.platform ");
+  console.log("process.platform == ", process.platform);
+
+
   res.setHeader('Content-Type', 'text/plain')
   res.end('hello')
 })
