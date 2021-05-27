c7d20223c662890a1cccacd5c083639877a964c3
yunisuthn <yunis.thn@gmail.com>
2021-05-27 14:56:48 +0300

    test9

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const path = require("path");
 const fs = require('fs');
 
-if(process.platform == "win32") { dir_home = process.env["USERPROFILE"]}
-if(process.platform  != "win32") {dir_home= process.env["$HOME"]} //process.env.$HOME};
+// if(process.platform == "win32") { dir_home = process.env["USERPROFILE"]}
+// if(process.platform  != "win32") {dir_home= process.env["$HOME"]} //process.env.$HOME};
+var dir_home = path.resolve(process.env["$HOME"]);
 // var dir_home = process.env[process.platform !="win32"?"HOME":"USERPROFILE"];
 
 console.log("process.platform == ", dir_home);
