558442257938cf96884e453d40f53024e41dcc36
yunisuthn <yunis.thn@gmail.com>
2021-05-27 09:25:30 +0300

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("path");
+const fs = require('fs');
+// var dir_home = process.env[process.platform =="win32"?"USERPROFILE":"HOME"];
+
+// console.log("di_home == ", dir_home);
+
+
+// fs.mkdirSync(path.join(dir_home,"Desktop", "Geeks"));
+app = express()
+
+
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('hello')
+})
+
+
+var port = process.env.PORT || 8081;
+app.listen(port);
